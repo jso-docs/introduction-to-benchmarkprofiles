d8ec1afb43ea31eb12df466dc88475b61c90f3a9
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2022-03-15 22:44:43 +0100

    Copy tutorial from package repo

--- index.jl
+++ index.jl
@@ -1,34 +1,46 @@
-# # Template for jso-docs separate pages
-
-# \toc
-
-# Modify this file
-
-# ## Testing
-
-# Testing
-
-A = rand(5, 5)
-b = A * ones(5)
-x = A \ b
-
-# Testing
-
-using LinearAlgebra
-norm(A * x - b), norm(x .- 1)
-
 #=
-## Plotting
 
-Use `joinpath("__site/assets", filename)` as the path to save the image in the right place and use `{{ rfig filename.ext Caption of image }}` to add the image afterwards.
-It is a good idea to use `# hide` so the save command doesn't appear. See the example below.
+This tutorial is essentially a collection of examples.
+
+## Performance Profile
+
+Performance profiles are straightforward to use. The input is a matrix `T` with entries `T[i,j]` indicating the cost to solve problem `i` using solver `j`. Cost can be, for instance, elapsed time, or number of evaluations. The cost should be positive. If any cost is zero, all measures will be shifted by 1.
+
+Basic usage:
+
 =#
+using BenchmarkProfiles, Random, Plots
 
+Random.seed!(0)
+
+T = 10 * rand(25, 3)
+performance_profile(PlotsBackend(), T, ["Solver 1", "Solver 2", "Solver 3"])
+#=
+
+A negative or infinite cost indicates a failure:
+
+=#
+T[2:20,1] .= Inf
+performance_profile(PlotsBackend(), T, ["Solver 1", "Solver 2", "Solver 3"])
+#=
+
+Here's an example with a strongly superior solver.
+
+=#
+T[:,2] = 100T[:,3]
+performance_profile(PlotsBackend(), T, ["Solver 1", "Solver 2", "Solver 3"])
+#=
+
+`Plots` arguments can be passed to `performance_profile()` or used as they normally would be with `Plots`.
+In the example below, we pass `xlabel` to `performance_profile` and set `ylabel` through `ylabel!`.
+
+=#
 using Plots
 
-x = rand(3)
-y = rand(3)
-plot(x, y)
-png(joinpath("__site/assets", "myplot")) # hide
+T = 10 * rand(25, 3)
+performance_profile(PlotsBackend(), T, ["Solver 1", "Solver 2", "Solver 3"],
+      lw=2, c=:black, linestyles=[:solid, :dash, :dot], xlabel="\\tau")
+ylabel!("\\rho(\\tau)")
+#=
 
-# {{ rfig myplot.png Example of plot and description }}
+=#
