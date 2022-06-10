8dd4eabea3b6ba4e9650a7a42d352bd4dcd0fd3a
abelsiqueira <nepper271@gmail.com>
2022-06-10 08:28:12 +0000

    Deploying to gh-pages from  @ ca13c38b1c2d399cf62e2848e1e6c71c44630b0f 🚀

--- assets/literate/index_script.jl
+++ assets/literate/index_script.jl
@@ -20,7 +20,7 @@ using Plots
 
 T = 10 * rand(25, 3)
 performance_profile(PlotsBackend(), T, ["Solver 1", "Solver 2", "Solver 3"],
-      lw=2, c=:black, linestyles=[:solid, :dash, :dot], xlabel="\\tau")
-ylabel!("\\rho(\\tau)")
+      lw=2, c=:black, linestyles=[:solid, :dash, :dot], xlabel="τ")
+ylabel!("ρ(τ)")
 png(joinpath("__site/assets", "pp4")) # hide
 
